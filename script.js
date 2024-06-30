fee13ffb50aa285bd24c3fde10374ee56edd271f
Elexis Falceso <160375645+Ashihiro21@users.noreply.github.com>
2024-06-30 11:27:15 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // script.js
 document.getElementById('downloadButton').addEventListener('click', function() {
     // Replace 'resume.pdf' with the actual path to your resume file
-    var fileUrl = 'Elexis Falceso_Resume_CV.pdf';
+    var fileUrl = 'Elexis_Falceso_Resume1_CV.pdf';
     
     var a = document.createElement('a');
     a.href = fileUrl;
-    a.download = 'Elexis Falceso_Resume_CV.pdf';
+    a.download = 'Elexis_Falceso_Resume1_CV.pdf';
     document.body.appendChild(a);
     a.click();
     document.body.removeChild(a);
